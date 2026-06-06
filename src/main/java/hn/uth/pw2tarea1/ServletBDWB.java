80d241c10404426a5c708a3ded855139531e3314
William Eduardo Banegas Cruz <williambanegas95@gmail.com>
2026-06-06 15:36:44 -0600

    William Banegas BinarioDecimal
    Agregando inicio bdwb.html
    Agregando ServletBDWB
    Vinculando MenuPrincipal al inicio bdwb

---
+++ src/main/java/hn/uth/pw2tarea1/ServletBDWB.java
@@ -0,0 +1,56 @@
+package com.example.prueba;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "convertidor", value = "/convertidor")
+public class ServletBDWB extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        String operation = request.getParameter("operation");
+
+        try {
+            if ("DecimalaBinario".equals(operation)) {
+            int decimal = Integer.parseInt(request.getParameter("number"));
+            String binary = Integer.toBinaryString(decimal);
+
+            out.println("<h2>Resultado</h2>");
+            out.println("<p>Decimal: " + decimal + "</p>");
+            out.println("<p>Binario: " + binary + "</p>");
+
+            }
+            else if ("BinarioaDecimal".equals(operation)) {
+                String binary = request.getParameter("number");
+                int decimal = Integer.parseInt(binary, 2);
+
+                out.println("<h2>Resultado</h2>");
+                out.println("<p>Binario: " + binary + "</p>");
+                out.println("<p>Decimal: " + decimal + "</p>");
+            } else {
+                out.println("<h3>Operación no válida</h3>");
+            }
+
+        } catch (Exception e) {
+            out.println("<h3>Error en la conversión</h3>");
+        }
+
+        out.println("<br><a href='dbwb.html'>Volver</a>");
+    }
+
+    @Override
+    public void init() {
+    }
+
+    @Override
+    public void destroy() {
+    }
+}
